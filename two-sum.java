712bf2649b686f592e0d259a39dadadc02cc1952
gautam725059 <guptagautam7250@gmail.com>
2026-03-21 03:11:20 +0530

    two-sum

---
+++ two-sum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
